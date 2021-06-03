bee2c47e4f094e2382cf98f105630f82418b8d40
mijkolsmith <mijkolsmith@live.com>
2021-06-03 18:54:49 +0200

    small edit

---
+++ Assets/Scripts/EndTurn.cs
@@ -0,0 +1,10 @@
+public class EndTurn : MonoBehaviour
+{
+    public void NextTurn()
+    {
+        if (GameManager.Instance.battleManager.playerMove == true)
+        {
+            GameManager.Instance.battleManager.NextTurn();
+        }
+    }
+}
